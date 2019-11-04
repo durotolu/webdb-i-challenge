f3f14970c4c859cbf6ef1edb55f3603e44004f27
Modurotolu Olokode <durolawk@gmail.com>
2019-11-04 15:43:12 +0100

    built accounts endpoints

---
+++ accountRouter.js
@@ -0,0 +1,57 @@
+const express = require('express');
+
+const db = require('./data/dbConfig.js');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    db('accounts')
+        .then(accounts => {
+            res.json(accounts);
+        })
+        .catch(error => {
+            res.status(500).json ({ message: 'something went wrong accessing data' + error.message });
+        });
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+        const account = await db('accounts').where({ id: req.params.id });
+        res.json(account[0]);
+    } catch (error) {
+        res.status(500).json ({ message: 'something went wrong accessing data' + error.message });
+    }
+})
+
+router.post('/', (req, res) => {
+    db('accounts').insert({ name: req.body.name, budget: req.body.budget })
+        .then(newId => {
+            res.json('New account created with id of ' + newId);
+        })
+        .catch(error => {
+            res.status(500).json ({ message: 'something went wrong accessing data' + error.message });
+        })
+})
+
+router.put('/:id', async (req, res) => {
+    try {
+        const result = await db('accounts').where({ id: req.params.id }).update({
+                            name: req.body.name,
+                            budget: req.body.budget
+                        });
+        res.json(result + ' account(s) got updated');
+    } catch (error) {
+        res.status(500).json ({ message: 'something went wrong accessing data' + error.message });
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const result = await db('accounts').where({ id: req.params.id }).del();
+        res.json(result + ' succeeded');
+    } catch (error) {
+        res.status(500).json ({ message: 'something went wrong accessing data' + error.message });
+    }
+})
+
+module.exports = router;
